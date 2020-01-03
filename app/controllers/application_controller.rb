8323b373fda85a891bdee9caa90a035c5c59059e
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-01-03 03:46:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,9 @@ class ApplicationController < Sinatra::Base
     @article = Article.find(params[:id])
     erb :show
   end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
 end
